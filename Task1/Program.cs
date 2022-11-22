99475d03e3d7940b8b32a915a58aea52c45e67d3
Mariya Andreeva <mashanasha_1@example.cdotnet>
2022-11-22 18:15:54 +0600

    Solving with a function

---
+++ Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6*/
+
+int Read(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+int Exam (int n)
+{
+    int s = n / 100;
+    return s;
+}
+
+int number = Read("Введите целое число: ");
+if(Exam (number) >= 1 || Exam (number) <= (-1))
+{
+Console.WriteLine(((number % 1000) - number % 100) / 100);
+}
+else
+{
+Console.WriteLine ("Третьей цифры нет");
+}
